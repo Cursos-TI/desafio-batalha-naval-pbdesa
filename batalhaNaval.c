dc0dbbe6996c95fe6ee577994fb6774470963b2b
pbdesa <57603352+pbdesa@users.noreply.github.com>
2025-10-25 21:55:21 +0000

    Novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,14 +1,76 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
+// Desafio Batalha Naval - MateCheck - Pablo Francisco de Sa
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
     // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+
+    //Declaração do tabuleiro 10x10 inicializado com zeros
+    int tabuleiro [10][10] = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+    //Declaração dos navios com seus tamanhos
+    int navio1 [3];
+    int navio2 [3];
+
+    // Navio 1: Horizontal
+    int linha_navio1 = 0;
+    int coluna_navio1 = 3;
+
+    // Navio 2: Vertical
+    int linha_navio2 = 6;
+    int coluna_navio2 = 7;
+
+    // --- Colocação dos navios no tabuleiro ---
+    // Verificar se o Navio 1 está dentro dos limites do tabuleiro
+    if(linha_navio1 >= 10  || coluna_navio1 + 2 >= 10) {
+        printf("ERRO: Navio 1 fora dos limites do tabuleiro.\n");
+        return 1;
+            }
+    
+    // Colocação do Navio 1 (horizontal)
+    for(int i = 0; i < 3; i++) {
+        tabuleiro[linha_navio1][coluna_navio1 + i] = 3; 
+    }   
+
+    // Verificar se o Navio 2 está dentro dos limites do tabuleiro
+    if(linha_navio2 + 2 >= 10 || coluna_navio2 >= 10) {
+        printf("ERRO: Navio 2 fora dos limites do tabuleiro.\n");
+        return 1;
+            }   
+
+    // Verificar sobreposição dos navios
+    for(int m = 0; m < 3; m++) {
+        if (tabuleiro[linha_navio2 + m][coluna_navio2] == 3) {
+            printf("ERRO: Navio 1 e 2 com sobreposição.\n");
+            return 1; 
+        }
+    }
+
+    // Colocação do Navio 2 (vertical)
+    for(int i = 0; i < 3; i++) {
+        tabuleiro[linha_navio2 + i][coluna_navio2] = 3; 
+    }
+
+    // Impressão do tabuleiro
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }   
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
